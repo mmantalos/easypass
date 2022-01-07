5f03e20be04215f707c5dbdc77701ca1033e4d58
roeczak <akoukas1911@gmail.com>
2022-01-07 02:00:58 +0200

    fixed date format in passesCost

--- api/endpoints/passesCost.js
+++ api/endpoints/passesCost.js
@@ -22,7 +22,7 @@ function getPassesCost(req,res){
     	console.log("Connected!");
       date_from = date_from.slice(0,4)+"-"+date_from.slice(4,6)+"-"+date_from.slice(-2);
       date_to = date_to.slice(0,4)+"-"+date_to.slice(4,6)+"-"+date_to.slice(-2);
-    	let myquery = `SELECT COUNT(p.pass_id) AS PassesCount, SUM(p.charge) AS PassesCost FROM vehicles AS v, stations AS s, passes AS p WHERE v.vehicle_id = p.vehicle_ref AND s.station_id = p.station_ref AND v.tag_provider = ${req.params["op2_ID"]} AND s.station_provider = ${req.params["op1_ID"]} AND CAST(p.timestamp AS date) BETWEEN ${req.params["date_from"]} AND ${req.params["date_to"]};
+    	let myquery = `SELECT COUNT(p.pass_id) AS PassesCount, SUM(p.charge) AS PassesCost FROM vehicles AS v, stations AS s, passes AS p WHERE v.vehicle_id = p.vehicle_ref AND s.station_id = p.station_ref AND v.tag_provider = ${req.params["op2_ID"]} AND s.station_provider = ${req.params["op1_ID"]} AND CAST(p.timestamp AS date) BETWEEN ${req.params["date_from"]} AND ${req.params["date_to"]};`;
         console.log(myquery);
         con.query(myquery, function (err, result, fields){
         	     if (err) throw err;
