6b688b2d3feaeea31568a2780448d002334380e8
Yiannis1999 <yiannis.tsouloukas@gmail.com>
2022-01-26 20:18:00 +0200

    Update passesCost.js

--- api/endpoints/passesCost.js
+++ api/endpoints/passesCost.js
@@ -4,80 +4,80 @@ var mysql = require('mysql');
 var moment = require('moment');
 const j2c = require('json2csv');
 
-function getPassesCost(req,res){
+function getPassesCost(req, res) {
   //get current date string with format "yyyy-mm-dd hh:mm:ss" from date object
   var reqTmstmp = moment(new Date()).format('YYYY-MM-DD hh:mm:ss')
   var date_from = req.params["date_from"];
   var date_to = req.params["date_to"];
   //check if input dates have "YYYYMMDD" format and in that case convert them to YYYY-MM-DD
   if (moment(date_from, 'YYYYMMDD', true).isValid() && moment(date_to, 'YYYYMMDD', true).isValid()) {
-      date_from = moment(date_from).format('YYYY-MM-DD');
-      date_to = moment(date_to).format('YYYY-MM-DD');
+    date_from = moment(date_from).format('YYYY-MM-DD');
+    date_to = moment(date_to).format('YYYY-MM-DD');
   }
   else {
-      res.status(400);
-      res.send('Please, give valid dates with format YYYYMMDD.');
-      return;
+    res.status(400);
+    res.send('Please, give valid dates with format YYYYMMDD.');
+    return;
 
-      console.log(date_from);
-      console.log(date_to);
+    console.log(date_from);
+    console.log(date_to);
 
 
     var con = mysql.createConnection({
-    host: "localhost",
-    user: "admin",
-    password: "freepasses4all",
-    database:"easy_pass",
-    timezone: "eet"
+      host: "localhost",
+      user: "admin",
+      password: "freepasses4all",
+      database: "easy_pass",
+      timezone: "eet"
     });
 
     //Make database connection and query. Error handling???
-    con.connect(function(err) {
+    con.connect(function (err) {
       if (err) {
-          res.status(500); // internal server error
-          res.send("DB connection refused.");
-          return;
+        res.status(500); // internal server error
+        res.send("DB connection refused.");
+        return;
       };
-    	console.log("Connected!");
+      console.log("Connected!");
 
-    	let myquery = `SELECT COUNT(p.pass_id) AS PassesCount, SUM(p.charge) AS PassesCost FROM vehicles AS v, stations AS s, passes AS p WHERE v.vehicle_id = p.vehicle_ref AND s.station_id = p.station_ref AND v.tag_provider = "${req.params["op2_ID"]}" AND s.station_provider = "${req.params["op1_ID"]}" AND CAST(p.timestamp AS date) BETWEEN "${req.params["date_from"]}" AND "${req.params["date_to"]}";`;
-      con.query(myquery, function (err, result, fields){
-          if (err) {
-              res.status(500); // internal server error
-              res.send("Query error.");
-              return;
-          }
-            var output = {
-                    op1_ID : req.params["op1_ID"],
-                    op2_ID : req.params["op2_ID"],
-                    RequestTimestamp : reqTmstmp,
-                    PeriodFrom : req.params["date_from"],
-                    PeriodTo : req.params["date_to"],
-                    NumberOfPasses : result.PassesCount,
-                    PassesCost : result.PassesCost
-                  }
-                  if (result.length == 0) {
-                      res.status(402); // no data
-                      res.send(output);
-                      return;
-                  }
-                  if(req.query.format=='json' || req.query.format==undefined){
-                              res.send(output);
-                          }else if(req.query.format=='csv'){
-                            j2c.json2csv(result,
-                                function (err, csv) {
-                                  if (err) {
-                                      res.status(500); // internal server error
-                                      res.send("CSV error.");
-                                      return;
-                                    }
-                                    res.attachment("PassesCost.csv").send(csv);
-                                }, { "delimiter": { "field": ';' } });
-                        }
-                    });
-        con.end();
+      let myquery = `SELECT COUNT(p.pass_id) AS PassesCount, SUM(p.charge) AS PassesCost FROM vehicles AS v, stations AS s, passes AS p WHERE v.vehicle_id = p.vehicle_ref AND s.station_id = p.station_ref AND v.tag_provider = "${req.params["op2_ID"]}" AND s.station_provider = "${req.params["op1_ID"]}" AND CAST(p.timestamp AS date) BETWEEN "${req.params["date_from"]}" AND "${req.params["date_to"]}";`;
+      con.query(myquery, function (err, result, fields) {
+        if (err) {
+          res.status(500); // internal server error
+          res.send("Query error.");
+          return;
+        }
+        var output = {
+          op1_ID: req.params["op1_ID"],
+          op2_ID: req.params["op2_ID"],
+          RequestTimestamp: reqTmstmp,
+          PeriodFrom: req.params["date_from"],
+          PeriodTo: req.params["date_to"],
+          NumberOfPasses: result.PassesCount,
+          PassesCost: result.PassesCost
+        }
+        if (result.length == 0) {
+          res.status(402); // no data
+          res.send(output);
+          return;
+        }
+        if (req.query.format == 'json' || req.query.format == undefined) {
+          res.send(output);
+        } else if (req.query.format == 'csv') {
+          j2c.json2csv(result,
+            function (err, csv) {
+              if (err) {
+                res.status(500); // internal server error
+                res.send("CSV error.");
+                return;
+              }
+              res.attachment("PassesCost.csv").send(csv);
+            }, { "delimiter": { "field": ';' } });
+        }
+      });
+      con.end();
     });
+  }
 }
-
-router.get('/PassesCost/:op1_ID/:op2_ID/:date_from/:date_to',getPassesCost);
+router.get('/PassesCost/:op1_ID/:op2_ID/:date_from/:date_to', getPassesCost);
 module.exports = router;
