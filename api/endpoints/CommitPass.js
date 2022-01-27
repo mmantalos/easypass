a851186498f1b58eccd4280d2d21d8b9cf32fc7f
Yiannis1999 <yiannis.tsouloukas@gmail.com>
2022-01-27 04:59:12 +0200

    Added error messages

--- api/endpoints/CommitPass.js
+++ api/endpoints/CommitPass.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 var mysql = require('mysql');
 
-function CommitPass(req,res){
+function CommitPass(req, res) {
     var timestamp = req.params["timestamp"];
     var date_ob = new Date();
     const date = ("0" + date_ob.getDate()).slice(-2);
@@ -10,42 +10,46 @@ function CommitPass(req,res){
     const year = date_ob.getFullYear();
     const hours = ("0" + date_ob.getHours()).slice(-2);
     const minutes = ("0" + date_ob.getMinutes()).slice(-2);
-    const seconds = ("0"+date_ob.getSeconds()).slice(-2);
+    const seconds = ("0" + date_ob.getSeconds()).slice(-2);
     var reqTmstmp = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
 
     var con = mysql.createConnection({
-    host: "localhost",
-    user: "admin",
-    password: "freepasses4all",
-    database:"easy_pass"
+        host: "localhost",
+        user: "admin",
+        password: "freepasses4all",
+        database: "easy_pass"
     });
 
     //Make database connection and query. Error handling???
-    con.connect(function(err) {
-    	if (err) throw err;
-    	console.log("Connected!");
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
         // convert input dates from "yyyymmdd" to "yyyy-mm-dd" format
-        timestamp = timestamp.slice(0,4)+"-"+timestamp.slice(4,6)+"-"+timestamp.slice(6,8)+" "+timestamp.slice(8,10)+":"+timestamp.slice(10,12)+":"+timestamp.slice(12,14);
+        timestamp = timestamp.slice(0, 4) + "-" + timestamp.slice(4, 6) + "-" + timestamp.slice(6, 8) + " " + timestamp.slice(8, 10) + ":" + timestamp.slice(10, 12) + ":" + timestamp.slice(12, 14);
 
-    	let myquery = `INSERT INTO passes(pass_id,timestamp,station_ref,vehicle_ref,charge) VALUES ('${req.params["pass_ID"]}','${req.params["timestamp"]}','${req.params["station_ID"]}','${req.params["vehicle_ID"]}',${req.params["euros"]}.${req.params["cents"]});`;
+        let myquery = `INSERT INTO passes(pass_id,timestamp,station_ref,vehicle_ref,charge) VALUES ('${req.params["pass_ID"]}','${req.params["timestamp"]}','${req.params["station_ID"]}','${req.params["vehicle_ID"]}',${req.params["euros"]}.${req.params["cents"]});`;
         //console.log(myquery);
 
-        con.query(myquery, function (err, result, fields){
-        		if (err) throw err;
-                var output = {
-                    pass_ID : req.params["pass_ID"],
-                    RequestTimestamp : reqTmstmp,
-                    Passtimestamp : req.params["timestamp"],
-                    station_ID : req.params["station_ID"],
-                    vehicle_ID : req.params["vehicle_ID"],
-                    charge : parseFloat(req.params["euros"]+req.params["cents"]),
-                    Result : result
-                }
-        		res.send(output);
-        	});
+        con.query(myquery, function (err, result, fields) {
+            if (err) {
+                res.status(500);
+                res.send({"status": "failed"});
+                return;
+            }
+            var output = {
+                pass_ID: req.params["pass_ID"],
+                RequestTimestamp: reqTmstmp,
+                Passtimestamp: req.params["timestamp"],
+                station_ID: req.params["station_ID"],
+                vehicle_ID: req.params["vehicle_ID"],
+                charge: parseFloat(req.params["euros"] + req.params["cents"]),
+                Result: result
+            }
+            res.send(output);
+        });
         con.end();
     });
 }
 
-router.get('/CommitPass/:pass_ID/:timestamp/:station_ID/:vehicle_ID/:euros.:cents', CommitPass);
+router.post('/CommitPass/:pass_ID/:timestamp/:station_ID/:vehicle_ID/:euros.:cents', CommitPass);
 module.exports = router;
