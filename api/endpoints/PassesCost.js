dcec77be7bea857fd8d86a14322f9592afa6db74
roeczak <akoukas1911@gmail.com>
2022-02-04 22:18:45 +0200

    added model error handling

--- api/endpoints/PassesCost.js
+++ api/endpoints/PassesCost.js
@@ -15,10 +15,10 @@ function getPassesCost(req, res) {
     date_to = moment(date_to).format('YYYY-MM-DD');
   }
   else {
-    res.status(400);
-    res.send('Please, give valid dates with format YYYYMMDD.');
-    return;
-}
+      res.status(400); // bad request
+      res.send({ "status": "failed", "description": "Date format should be YYYYMMDD." });
+      return;
+  }
     console.log(date_from);
     console.log(date_to);
     date_fr=moment(req.params.date_from,'YYYYMMDD').format('YYYY-MM-DD');
@@ -35,18 +35,18 @@ function getPassesCost(req, res) {
     //Make database connection and query. Error handling???
     con.connect(function (err) {
       if (err) {
-        res.status(500); // internal server error
-        res.send("DB connection refused.");
-        return;
-      };
+          res.status(500); // internal server error
+          res.send({ "status": "failed", "description": "Query error." });
+          return;
+      }
       console.log("Connected!");
 
       let myquery = `SELECT COUNT(p.pass_id) AS PassesCount, SUM(p.charge) AS PassesCost FROM vehicles AS v, stations AS s, passes AS p WHERE v.vehicle_id = p.vehicle_ref AND s.station_id = p.station_ref AND v.tag_provider = "${req.params["op2_ID"]}" AND s.station_provider = "${req.params["op1_ID"]}" AND CAST(p.timestamp AS date) BETWEEN "${req.params["date_from"]}" AND "${req.params["date_to"]}";`;
       con.query(myquery, function (err, result, fields) {
         if (err) {
-          res.status(500); // internal server error
-          res.send("Query error.");
-          return;
+            res.status(500); // internal server error
+            res.send({ "status": "failed", "description": "Query error." });
+            return;
         }
         var output = {
           op1_ID: req.params["op1_ID"],
@@ -58,9 +58,9 @@ function getPassesCost(req, res) {
           PassesCost: result[0].PassesCost
         }
         if (result.length == 0) {
-          res.status(402); // no data
-          res.send(output);
-          return;
+            res.status(402); // no data
+            res.send({ "status": "failed", "description": "No data." });
+            return;
         }
         if (req.query.format == 'json' || req.query.format == undefined) {
           res.send(output);
@@ -69,18 +69,17 @@ function getPassesCost(req, res) {
           converter.json2csv(result,
             function (err, csv) {
               if (err) {
-                res.status(500); // internal server error
-                res.send("CSV error.");
-                return;
+                  res.status(500); // internal server error
+                  res.send({ "status": "failed", "description": "Convertion error." });
+                  return;
               }
               res.attachment("PassesCost.csv").send(csv);
             }, { "delimiter": { "field": ';' } });
         }
         else {
-           res.status(400);
-           res.send({ "status": "failed" });
-           return;
-       }
+            res.status(400); // bad request
+            res.send({ "status": "failed", "description": "Format should be json or csv." });
+        }
       });
       con.end();
     });
