f876c12468f050d376829f85010a2ea28f3b528a
DimiDespot <el17066@mail.ntua.gr>
2022-02-23 23:31:37 +0200

    deleted .sh

--- test-cli/test-passesanalysis.sh
+++
@@ -1,9 +0,0 @@
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo 'Test1 - This test should display results in json format'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-`dirname $0`/../cli/se2162 passesanalysis --format json --op1 aodos --op2 olympia_odos --datefrom 20200101 --dateto 20200131
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo 'Test2 - This test should display results in csv format'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-`dirname $0`/../cli/se2162 passesanalysis --format csv --op1 aodos --op2 kentriki_odos --datefrom 20200301 --dateto 20200331
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
--- test-cli/test-passesupd.sh
+++
@@ -1,4 +0,0 @@
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo 'Test'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-`dirname $0`/../cli/se2162 admin --passesupd --source `dirname $0`/../backend/sampledata01/sampledata01_passes100_8000.csv
